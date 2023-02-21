f95c93c94a26f6d3a131c9108d2647eb94a2587c
root <root@file.haus>
2023-02-21 14:06:42 +0000

    Changes to be committed: 	new file:   .gitignore 	new file:   index.php 	new file:   sitefiles/css.css 	new file:   vdl.php 	new file:   vdl.php.stable.backup

---
+++ index.php
@@ -0,0 +1,14 @@
+<html>
+ <head>
+  <link rel="stylesheet" href="sitefiles/css.css">
+  <title>delphium downloader</title>
+ </head>
+
+ <body>
+ <h1>delphium downloader</h1>
+<form action="vdl.php" method="post">
+video url: <input type="text" name="url"><br>
+<input type="submit">
+</form>
+</body>
+</html>
---
+++ vdl.php
@@ -0,0 +1,25 @@
+<?php require __DIR__.'/vendor/autoload.php'; $yt = new \YoutubeDl\YoutubeDl(); 
+$yt->setBinPath('/usr/local/bin/yt-dlp'); $yt->debug(function ($type, $buffer) {
+    if (\Symfony\Component\Process\Process::ERR === $type) { echo $buffer;
+    } else {
+        echo $buffer;
+    }
+});
+$options = \YoutubeDl\Options::create() 
+    ->downloadPath(__DIR__.'/dls')
+    ->output('%(epoch)s.%(ext)s')
+    ->url($_POST["url"]);
+$collection = $yt->download($options); echo "Download completed\n";
+
+foreach ($collection->getVideos() as $video) {
+    if ($video->getError() !== null) {
+        echo "Error downloading video: {$video->getError()}.";
+    } else {
+        echo $video->getEpoch(); // Will return Phonebloks
+        // $video->getFile(); // \SplFileInfo instance of downloaded file
+    }
+}
+
+header("Location: dls/".urlencode($video->getEpoch()).".".urlencode($video->getExt()));
+
+// header('Location: https://delphium.jesus.fish/dls/' . http_build_query($video->getEpoch() . '.webm');
