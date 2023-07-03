d56dbfef60fe2011c4fa5425deba0ee52111343a
phin <112971806+phinwww@users.noreply.github.com>
2023-07-03 14:54:56 +0000

    Update index.php

--- index.php
+++ index.php
@@ -8,8 +8,6 @@
  <h1>delphium downloader</h1>
 <form action="vdl.php" method="post">
 video url: <input type="text" name="url"><br>
-// <input type="radio" id="mp4" name="mp4" value="mp4">
-//     <label for="mp4">Re-encode as mp4</label><br>
 <input type="radio" id="quality" name="quality" value="max" checked="checked">
     <label for="max">Max Quality</label>
 <input type="radio" id="quality" name="quality" value="1080">
